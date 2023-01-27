fe84d4deb9d0a6b7eb2a9c064cf64edc8b900c35
rshahid26 <114392205+rshahid26@users.noreply.github.com>
2023-01-27 09:45:54 -0500

    mouseover

--- pixel.js
+++ pixel.js
@@ -1,47 +1,123 @@
-import {data} from "./data.js"
+import {chartData} from "./data.js"
 
 const image = document.getElementById("image");
+/*
+function createCanvas() {
+    let myImg = new Image();
+    myImg.src = 'vxx%20vxxiv.PNG';
+    let context = document.getElementById('image_canvas').getContext('2d');
+    context.drawImage(myImg, 0, 0);
+    let pixel = context.getImageData(x, y, 1, 1).data;
+}
+createCanvas();
+
+ */
+
 const chart = {
     origin: {
         x: 43,
         y: 48,
         xVal: 930,
-        yVal: 23.96
+        yVal: 25.23
     },
     dimensions: {
         x: 715,
-        y: 1765
+        y: 1715
     },
     scale: {
         x: 14 * 60, // in MINUTES
-        y: 18.06 // in DOLLARS
+        y: 2.82 // in DOLLARS
     }
 }
 
-image.addEventListener("click", (e) => {
+/*
+let array = data.split("image.html?_ijt=kar3nu0klhfkcrargbp9dib3ai&_ij_reload=RELOAD_ON_SAVE:46 ").map(n => n.replace(/(\r\n|\n|\r|0000000000001|0000000000002|0000000000003|0000000000004|0000000000005|0000000000006|0000000000007|0000000000008|0000000000009)/gm, "")).map(n => [n.split(" ")[0], n.split(" ")[1]]);
+let previousTime = 0;
+for (let key in array) {
 
-    let coordinate = {
-        x: Math.abs(image.getBoundingClientRect().left - e.clientX) - chart.origin.x,
-        y: Math.abs(image.getBoundingClientRect().bottom - e.clientY) - chart.origin.y,
+    if (Number(array[key][0]) === previousTime) delete array[key];
+    else previousTime = Number(array[key][0]);
+
+}
+let newArray = [];
+array.forEach(n => newArray.push(n));
+
+ */
+
+let chartData2 = cleanData(chartData);
+console.log(chartData);
+
+function oneToTwo(data) {
+
+    let keyArray = [];
+    for (let key in data) {
+
+        for (let time of keyArray) {
+            if (time === data[key][0]) console.log(time);
+        }
+
+        keyArray.push(data[key][0]);
     }
+}
 
-    let timeStamp = Math.round(coordinate.x / (chart.dimensions.x / (chart.scale.x)));
-    let price = chart.origin.yVal + Math.round(coordinate.y / (chart.dimensions.y / chart.scale.y) * 100) / 100;
 
-    let array = data.split("image.html?_ijt=kar3nu0klhfkcrargbp9dib3ai&_ij_reload=RELOAD_ON_SAVE:46 ").map(n => n.replace(/(\r\n|\n|\r|0000000000001|0000000000002|0000000000003|0000000000004|0000000000005|0000000000006|0000000000007|0000000000008|0000000000009)/gm, "")).map(n => [n.split(" ")[0], n.split(" ")[1]]);
+createChart(chartData2);
+
+function cleanData(data) {
 
     let previousTime = 0;
-    for (let key in array) {
+    for (let key in data) {
 
-        if (Number(array[key][0]) === previousTime) delete array[key];
-        else previousTime = Number(array[key][0]);
+        data[key][1] = Number(data[key][1].toString().replace(/(\r\n|\n|\r|0000000000001|0000000000002|0000000000003|0000000000004|0000000000005|0000000000006|0000000000007|0000000000008|0000000000009)/gm, ""));
+
+        if (Number(data[key][0]) === previousTime) delete data[key];
+        else previousTime = Number(data[key][0]);
 
     }
 
-    let newArray = [];
-    array.forEach(n => newArray.push(n));
+    let tempData = [];
+    data.forEach(n => tempData.push(n));
+    data = tempData;
 
-    console.log(newArray);
-    console.log(timeStamp, price);
+    return data;
 
-});
+}
+
+image.addEventListener("click", (e) => {
+    image.addEventListener("mousemove", (e) => {
+
+        let coordinate = {
+            x: Math.abs(image.getBoundingClientRect().left - e.clientX) - chart.origin.x,
+            y: Math.abs(image.getBoundingClientRect().bottom - e.clientY) - chart.origin.y,
+        }
+
+        let timeStamp = Math.round(coordinate.x / (chart.dimensions.x / (chart.scale.x)));
+        let price = chart.origin.yVal + Math.round(coordinate.y / (chart.dimensions.y / chart.scale.y) * 100) / 100;
+
+        console.log(timeStamp, price);
+        //data.push([timeStamp, price]);
+        cleanData();
+
+        //console.log(data);
+
+    });
+})
+
+
+function createChart(data) {
+
+    const ctx = document.getElementById("myChart");
+
+    let myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: 'first attempt',
+                data: data,
+                fill: false,
+                borderColor: 'rgb(81,248,60)',
+                tension: 0.1
+            }]
+        }
+    });
+}
