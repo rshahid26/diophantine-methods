ba31ed141a6293945f2200da545908776566451f
rshahid26 <114392205+rshahid26@users.noreply.github.com>
2023-02-01 01:50:04 -0500

    ensures every timestamp maps to one value only

--- pixel.js
+++ pixel.js
@@ -1,17 +1,6 @@
 import {chartData} from "./data.js"
 
 const image = document.getElementById("image");
-/*
-function createCanvas() {
-    let myImg = new Image();
-    myImg.src = 'vxx%20vxxiv.PNG';
-    let context = document.getElementById('image_canvas').getContext('2d');
-    context.drawImage(myImg, 0, 0);
-    let pixel = context.getImageData(x, y, 1, 1).data;
-}
-createCanvas();
-
- */
 
 const chart = {
     origin: {
@@ -30,38 +19,24 @@ const chart = {
     }
 }
 
-/*
-let array = data.split("image.html?_ijt=kar3nu0klhfkcrargbp9dib3ai&_ij_reload=RELOAD_ON_SAVE:46 ").map(n => n.replace(/(\r\n|\n|\r|0000000000001|0000000000002|0000000000003|0000000000004|0000000000005|0000000000006|0000000000007|0000000000008|0000000000009)/gm, "")).map(n => [n.split(" ")[0], n.split(" ")[1]]);
-let previousTime = 0;
-for (let key in array) {
-
-    if (Number(array[key][0]) === previousTime) delete array[key];
-    else previousTime = Number(array[key][0]);
-
-}
-let newArray = [];
-array.forEach(n => newArray.push(n));
-
- */
-
-let chartData2 = cleanData(chartData);
-console.log(chartData);
-
-function oneToTwo(data) {
-
-    let keyArray = [];
-    for (let key in data) {
-
-        for (let time of keyArray) {
-            if (time === data[key][0]) console.log(time);
-        }
-
-        keyArray.push(data[key][0]);
+let currData = [];
+image.addEventListener("click", (e) => {
+    let coordinate = {
+        x: Math.abs(image.getBoundingClientRect().left - e.clientX) - chart.origin.x,
+        y: Math.abs(image.getBoundingClientRect().bottom - e.clientY) - chart.origin.y,
     }
-}
 
+    let timeStamp = Math.round(coordinate.x / (chart.dimensions.x / (chart.scale.x)));
+    let price = chart.origin.yVal + Math.round(coordinate.y / (chart.dimensions.y / chart.scale.y) * 100) / 100;
 
-createChart(chartData2);
+    console.log(timeStamp, price);
+    currData.push([timeStamp, price]);
+    console.log(currData);
+})
+
+console.log(chartData);
+createChart(chartData);
+
 
 function cleanData(data) {
 
@@ -83,41 +58,61 @@ function cleanData(data) {
 
 }
 
-image.addEventListener("click", (e) => {
-    image.addEventListener("mousemove", (e) => {
-
-        let coordinate = {
-            x: Math.abs(image.getBoundingClientRect().left - e.clientX) - chart.origin.x,
-            y: Math.abs(image.getBoundingClientRect().bottom - e.clientY) - chart.origin.y,
-        }
-
-        let timeStamp = Math.round(coordinate.x / (chart.dimensions.x / (chart.scale.x)));
-        let price = chart.origin.yVal + Math.round(coordinate.y / (chart.dimensions.y / chart.scale.y) * 100) / 100;
-
-        console.log(timeStamp, price);
-        //data.push([timeStamp, price]);
-        cleanData();
-
-        //console.log(data);
-
-    });
-})
-
-
 function createChart(data) {
+
+    let dataX = [];
+    let dataY = [];
+    for (let element of data) {
+        dataX.push(element[0]);
+        dataY.push(element[1]);
+    }
 
     const ctx = document.getElementById("myChart");
 
     let myChart = new Chart(ctx, {
         type: 'line',
         data: {
+            labels: dataX,
             datasets: [{
-                label: 'first attempt',
-                data: data,
+                label: 'INAV',
+                data: dataY,
                 fill: false,
-                borderColor: 'rgb(81,248,60)',
-                tension: 0.1
+                borderColor: 'rgb(41,208,41)',
+                tension: 0.1,
+                pointRadius: 0
             }]
+        },
+        options: {
+            scales: {
+                x: {
+                    grid: {
+                        display: false
+                    }
+                },
+                y: {
+                    grid: {
+                        display: false
+                    }
+                }
+            }
         }
     });
+
+}
+
+function oneToTwo(data) {
+
+    let keyArray = [];
+    for (let key in data) {
+
+        for (let time of keyArray) {
+            if (time === data[key][0]) console.log(time + "oneToTwo");
+            if (time > data[key][0]) console.log(time + "non decreasing");
+
+        }
+
+        keyArray.push(data[key][0]);
+    }
+
+    return keyArray;
 }
