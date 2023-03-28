fcad5a3b3b90585e605f9e57ae5663345c50bda6
rshahid26 <114392205+rshahid26@users.noreply.github.com>
2023-03-28 01:21:29 -0400

    optimized

--- Number Theory/derichlet.js
+++ Number Theory/derichlet.js
@@ -1,10 +1,26 @@
+// Approximates any real number using the smallest possible integers p / q
+function findApproximate(realNum, error) {
+
+    let limit = Math.round(1 / error);
+    while (true) {
+
+        const multiples = findMultiple(realNum, limit, error);
+
+        if (Object.keys(multiples).length !== 0) {
+
+            const multiple = Object.keys(multiples)[0];
+            return Math.round(multiples[multiple]) + " / " + multiple +
+                " (within " + error + ")";
+        }
+        limit++;
+    }
+}
+
 // Derichlet's Approximation Theorem
-function findMultiples(realNum, limit, error) {
+function findMultiple(realNum, limit, error) {
 
     let fractionals = new Array(limit);
-    let answers = {};
-
-    error = !error || error > 1 / 1_000_000 ? 1_000_000 : error;
+    error = !error || error > 1 / 1_000_000 ? 1_000_000 : Math.round(1 / error);
 
     for (let i = 0; i < limit; i++) {
         fractionals[i] = (realNum * i) - Math.floor(realNum * i);
@@ -13,42 +29,10 @@ function findMultiples(realNum, limit, error) {
     for (let i = 0; i < limit; i++) {
         for (let j = i + 1; j <= limit; j++) {
 
-            if (Math.abs(fractionals[i] - fractionals[j]) < (1 / limit)) {
-
-                const index = Math.abs(i - j);
-                if (!answers[index]) answers[index] = Math.round(realNum * index * error) / error;
-            }
-
+            if (Math.abs(fractionals[i] - fractionals[j]) < (Math.abs(i - j) / limit))
+                return {[Math.abs(i - j)] : Math.round(realNum * Math.abs(i - j) * error) / error};
         }
     }
-    return answers;
 }
 
-// Approximates any real number using the smallest possible integers p / q
-function findApproximate(realNum, error) {
-
-    let approximates = {};
-    let limit = Math.ceil(1 / error);
-
-    while (Object.keys(approximates).length < 1) {
-        const multiples = findMultiples(realNum, limit, error);
-
-        console.log(limit, multiples);
-        limit++;
-
-        if (Object.keys(multiples).length !== 0) {
-
-            const multiple = Object.keys(multiples)[0];
-
-            if (multiples[multiple] - Math.floor(multiples[multiple]) > 0)
-                return Math.floor(multiples[multiple]) + " / " + multiple +
-                    " (within " + (1 / (Math.floor(multiples[multiple]) * limit)) + ")";
-
-            else return Math.ceil(multiples[multiple]) + " / " + multiple;
-
-        }
-    }
-
-    console.log(approximates);
-}
-console.log(findApproximate(Math.pow(2, 1/2), 0.00001));
+console.log(findApproximate(Math.pow(59, 7/9), 0.000001));
