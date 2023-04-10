0b44b5af2d00aa0cd419ec82adb3d9c0de993f1b
rshahid26 <114392205+rshahid26@users.noreply.github.com>
2023-04-09 21:52:38 -0400

    farey approximations

--- Number Theory/derichlet.js
+++ Number Theory/derichlet.js
@@ -1,13 +1,13 @@
 /**
  * Approximates any real number using integer values p, q where
- * p / q is within "error" distance of the real number.
+ * p / q is within "epsilon" distance of the real number.
  *
  * Uses Derichlet's Approximation Theorem to find the qth multiple of
  * the real number within 1/qN of the nearest integer. q's existence
  * is guaranteed by the pigeonhole principle.
  */
-function derichletApproximation(realNum, error) {
-    let limit = Math.round(1 / error);
+function derichletApproximation(realNum, epsilon) {
+    let limit = Math.round(1 / epsilon);
     let fractionals = [];
 
     while (true) {
@@ -18,7 +18,7 @@ function derichletApproximation(realNum, error) {
                 if (Math.abs(fractionals[i] - fractionals[j]) < (Math.abs(i - j) / limit)) {
                     const q = j - i;
                     const p = Math.round(q * realNum);
-                    return p + " / " + q + " (within " + error + ")";
+                    return p + " / " + q + " (within " + epsilon + ")";
                 }
             }
         }
@@ -30,10 +30,10 @@ function derichletApproximation(realNum, error) {
  * Finds the most optimal rational approximation of any real number using
  * a converging sequence of continued fractions.
  *
- * Terminates when the approximation is within "error" distance of the
+ * Terminates when the approximation is within "epsilon" distance of the
  * real number.
  */
-function continuedApproximation(realNum, error) {
+function continuedApproximation(realNum, epsilon) {
 
     let notation = [];
     let fractional = realNum;
@@ -45,8 +45,8 @@ function continuedApproximation(realNum, error) {
         notation.push(Math.floor(fractional));
 
         const fraction = evaluateContinuedFraction(notation, 0);
-        if (Math.abs(fraction[0] / fraction[1] - realNum) < error)
-            return fraction[0] + " / " + fraction[1] + " (within " + error + ")";
+        if (Math.abs(fraction[0] / fraction[1] - realNum) < epsilon)
+            return fraction[0] + " / " + fraction[1] + " (within " + epsilon + ")";
     }
 }
 
@@ -62,75 +62,3 @@ function evaluateContinuedFraction(notation, index) {
         return [notation[index] * fraction[0] + fraction[1], fraction[0]];
     }
 }
-
-function euclideanAlgorithm(a, b) {
-    if (b === 0) return a;
-    else return euclideanAlgorithm(b, a % b);
-}
-
-/**
- * Returns the Farey sequence of size n. These are all fractions
- * h / k where h, k are co-prime and 0 <= h <= k <= n.
- */
-function listFareySequenceBF(n, order) {
-    if (n < 1) return [];
-
-    let list = [];
-    for (let k = 0; k <= n; k++) {
-        for (let h = 0; h <= k; h++) {
-
-            // Excludes h = 0, k = 0 case
-            if (euclideanAlgorithm(h, k) === 1) list.push([h, k]);
-        }
-    }
-    return list.sort(order || fractionsAscending);
-}
-function listFareySequenceR(n) {
-
-}
-/**
- * Returns the Farey Sequence of size n, this time using the mediant
- * property where fraction [h,k] is equal to the mediant of its
- * nearest neighbors [... [a,b],[h,k],[c,d]...].
- */
-function listFareySequence(n) {
-    if (n < 1) return [];
-    let list = [[0, 1], [1, 1]];
-
-    let index = 0;
-    while (index < list.length - 1) {
-
-        const [a, b] = list[index];
-        const [c, d] = list[index + 1];
-
-
-
-        if (b + d <= n) list.splice(index + 1, 0, [a + c, b + d]);
-        else index++;
-    }
-    return list;
-}
-
-function findFareyNeighbors(numerator, denominator, n) {
-}
-
-const fareyAsymptote = (n) => Math.round(3 * Math.pow(n, 2) / Math.pow(Math.PI, 2));
-
-function fractionsAscending(frac1, frac2) {
-    return (frac1[0] * frac2[1]) - (frac1[1] * frac2[0]);
-}
-
-function fractionsDescending(frac1, frac2) {
-    return (frac1[1] * frac2[0]) - (frac1[0] * frac2[1]);
-}
-
-function factorial(n) {
-    if (n === 0) return 1;
-    else return n * factorial(n - 1);
-}
-
-// for (let n = 0; n < 10; n++) {
-//     console.log(n, listFareySequence(n).length, fareyAsymptote(n));
-// }
-
-listFareySequence(13);
---
+++ Number Theory/farey.js
@@ -0,0 +1,111 @@
+const fareyAsymptote = (n) => Math.round(3 * Math.pow(n, 2) / Math.pow(Math.PI, 2));
+
+/**
+ * Brute forces the Farey sequence of size n. These are all fractions
+ * h / k where h, k are co-prime and 0 <= h <= k <= n.
+ */
+function getFareySequenceBF(n, order) {
+    if (n < 1) return [];
+
+    let sequence = [];
+    for (let k = 0; k <= n; k++) {
+        for (let h = 0; h <= k; h++) {
+
+            // Excludes h = 0, k = 0 case
+            if (euclideanAlgorithm(h, k) === 1) sequence.push([h, k]);
+        }
+    }
+    return sequence.sort(order || fractionsAscending);
+}
+
+function euclideanAlgorithm(a, b) {
+    if (b === 0) return a;
+    else return euclideanAlgorithm(b, a % b);
+}
+
+function fractionsAscending(frac1, frac2) {
+    return (frac1[0] * frac2[1]) - (frac1[1] * frac2[0]);
+}
+
+function fractionsDescending(frac1, frac2) {
+    return (frac1[1] * frac2[0]) - (frac1[0] * frac2[1]);
+}
+
+/**
+ * Returns the Farey sequence of size n, this time using the mediant
+ * property where fraction [h,k] is equal to the mediant of its
+ * nearest neighbors.
+ *
+ * Meaning, for [...[a,b],[h,k],[c,d]...], h = a+c and k = b+d.
+ */
+function getFareySequence(n) {
+    if (n < 1) return [];
+    let sequence = [[0, 1], [1, 1]];
+
+    let index = 0;
+    while (index < sequence.length - 1) {
+
+        const [a, b] = sequence[index];
+        const [c, d] = sequence[index + 1];
+
+        if (b + d <= n) sequence.splice(index + 1, 0, [a + c, b + d]);
+        else index++;
+    }
+    return sequence;
+}
+/**
+ * Recursive solution for Farey sequences of small n.
+ */
+function getFareySequenceR(n) {
+    if (n === 0) return [];
+    if (n === 1) return [[0, 1], [1, 1]];
+    else {
+        let sequence = getFareySequenceR(n-1);
+        for (let index = 0; index < sequence.length - 1; index++) {
+            const [a, b] = sequence[index];
+            const [c, d] = sequence[index + 1];
+
+            if (b + d <= n) sequence.splice(index + 1, 0, [a + c, b + d]);
+        }
+        return sequence;
+    }
+}
+
+function getFareyNeighbors(numerator, denominator, n) {
+    let neighbors = {};
+
+    for (let b = n; b > 0; b--) {
+        for (let a = 0; a <= b; a++) {
+
+            if (numerator * b - denominator * a === -1) neighbors["after"] = [a, b];
+            if (numerator * b - denominator * a === 1) neighbors["before"] = [a, b];
+            if (Object.keys(neighbors).length > 1) return neighbors;
+        }
+    }
+}
+
+function fareyApproximation(realNum, n) {
+
+    const fractional = realNum - Math.floor(realNum);
+    const distance = (fraction) => {return Math.abs(fraction[0] / fraction[1] - fractional);}
+
+    const sequence = getFareySequence(n);
+    let record = Number.MAX_VALUE;
+
+    for (let i = 0; i < sequence.length; i++) {
+
+        if (distance(sequence[i]) > record)
+            return distance(sequence[i - 1]) <= distance(sequence[i]) ?
+                [sequence[i - 1][0] + Math.floor(realNum) * sequence[i - 1][1], sequence[i - 1][1]] :
+                [sequence[i][0] + Math.floor(realNum) * sequence[i][1], sequence[i][1]];
+
+        else record = distance(sequence[i]);
+    }
+}
+
+function factorial(n) {
+    if (n === 0) return 1;
+    else return n * factorial(n - 1);
+}
+
+console.log(fareyApproximation(Math.PI, 10000));
