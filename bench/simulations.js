656f509367a1a9c037eb1600fb88378eec5078bb
rshahid26 <114392205+rshahid26@users.noreply.github.com>
2023-04-27 06:07:07 -0400

    added rate of convergence tests

---
+++ bench/simulations.js
@@ -0,0 +1,96 @@
+import fs from 'fs';
+import { getContinuedFraction, continuedApproximation } from "../scripts/dirichlet.js";
+import { dirichletApproximation } from "../scripts/dirichlet.js";
+import { fareyApproximation } from "../scripts/farey.js";
+import { nearestFareyFraction } from "../scripts/farey.js";
+
+const testData = "bench/testdata.txt";
+const output = "bench/output.txt";
+
+function generateRealNums(count, min, max) {
+    const set = [];
+    for (let i = 0; i < count; i++)
+        set.push(Math.random() * (max - min) + min);
+
+    return set;
+}
+
+function updateTestData(count, min, max) {
+    const realNums = generateRealNums(count, min, max);
+    const storedRealNums = realNums.join('\n');
+
+    fs.writeFile(testData, storedRealNums, (err) => {
+        if (err) console.error(err);
+        else console.log('Random numbers saved to', testData);
+    });
+}
+
+function len(fraction) {
+    return fraction[0].toString().length + fraction[1].toString().length;
+}
+
+function simulate(method, realNums, epsilon, sample = realNums.length) {
+    const lengths = [];
+
+    for (let i = 0; i < sample; i++) {
+        lengths.push(len(method(realNums[i], epsilon)));
+    }
+    const sum = lengths.reduce((sum, value) => sum + value);
+    return sum / sample;
+}
+
+function publish(sample, epsilon, averageLengths) {
+
+    const line = "\nn: " + sample + " ε: " + epsilon + " | " + averageLengths;
+    fs.appendFile(output, line, (err) => {
+        if (err) console.error(err);
+        else console.log('lengths saved to', output);
+    });
+}
+
+/**
+ * Returns average time complexity of continued fraction approximations,
+ * which has complexity O(k^2). Use this to calculate k/n where n=1/epsilon,
+ * the typical input for time complexities in this package.
+ *
+ * Also measures the distribution of realNums elements. Low (<0.01%) rate
+ * of convergence means the sample is skewed towards rationals and quadratic
+ * irrationals.
+ */
+function getRateOfConvergence(realNums, epsilon, sample = realNums.length) {
+    const lengths = [];
+
+    for (let i = 0; i < sample; i++) {
+        lengths.push(getContinuedFraction(realNums[i], epsilon).length ** 2);
+        //if (lengths[i] > 100) console.log(lengths[i], realNums[i], getContinuedFraction(realNums[i], epsilon), continuedApproximation(realNums[i], epsilon));
+    }
+
+    return lengths.reduce((sum, value) => sum + value) / sample;
+}
+
+// fs.readFile(testData, 'utf-8', (err, data) => {
+//     if (err) console.error(err);
+//     else {
+//         const realNums = data.split('\n').map(Number);
+//         const epsilon = 0.000001;
+//         const sample = 100000;
+//
+//         const averageLengths = [];
+//         averageLengths.push(simulate(continuedApproximation, realNums, epsilon, sample));
+//         averageLengths.push(simulate(dirichletApproximation, realNums, epsilon, sample));
+//         averageLengths.push(simulate(fareyApproximation, realNums, epsilon, sample));
+//         // averageLengths.push(simulate(nearestFareyFraction, realNums, epsilon, sample));
+//
+//         publish(sample, epsilon, averageLengths);
+//     }
+// });
+
+fs.readFile(testData, 'utf-8', (err, data) => {
+    if (err) console.error(err);
+    else {
+        const realNums = data.split('\n').map(Number);
+        const epsilon = 0.000001;
+        const sample = 100000;
+        console.log(getRateOfConvergence(realNums, epsilon, sample));
+    }
+});
--- scripts/dirichlet.js
+++ scripts/dirichlet.js
@@ -30,25 +30,25 @@ export function dirichletApproximation(realNum, epsilon) {
 }
 
 /**
- * Finds the most optimal rational approximation of any real number using
- * a converging sequence of continued fractions.
+ * Takes any real number and approximates it using a converging sequence
+ * of continued fractions. Returns a shorthand notation containing just
+ * the floored integers in the approximation.
  *
  * Terminates when the approximation is within "epsilon" distance of the
  * real number.
  */
-export function continuedApproximation(realNum, epsilon) {
+export function getContinuedFraction(realNum, epsilon) {
     let notation = [];
     let fractional = realNum;
 
     notation.push(Math.floor(realNum));
-
     while (true) {
         fractional = 1 / (fractional - Math.floor(notation[notation.length - 1]));
         notation.push(Math.floor(fractional));
 
-        const fraction = evaluateContinuedFraction(notation, 0);
+        const fraction = evaluateContinuedFraction(notation);
         if (Math.abs(fraction[0] / fraction[1] - realNum) < epsilon)
-            return fraction;
+            return notation;
     }
 }
 
@@ -56,11 +56,19 @@ export function continuedApproximation(realNum, epsilon) {
  * Takes a continued (but finite) fraction written in shorthand notation
  * and simplifies it recursively.
  */
-function evaluateContinuedFraction(notation, index) {
+export function evaluateContinuedFraction(notation, index = 0) {
     if (index === notation.length - 1) {
         return [notation[index], 1];
     } else {
         const fraction = evaluateContinuedFraction(notation, index + 1);
         return [notation[index] * fraction[0] + fraction[1], fraction[0]];
     }
+}
+
+/**
+ * Returns the most optimal rational approximation of any real number
+ * one it's within "epsilon" distance.
+ */
+export function continuedApproximation(realNum, epsilon) {
+    return evaluateContinuedFraction(getContinuedFraction(realNum, epsilon));
 }
